f714020f32020638e209512b05dfcf1ec7bfc947
Cyrus Rome Dela Cruz <cdelacruz@pitx.com.ph>
2020-09-10 00:55:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Definition
+
+Javascript code to execute deep cloning on Objects and Array of Objects
+
+# Installation
+
+`npm i deepclone --save`
+
+Example...
+
+```
+const deepCloneMe = [{ a: 1, b: 2 }]
+const iAmYourClone = deepClone(deepCloneMe)
+
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+const deepClone = items => {
+  if (Array.isArray(items) && !items.length) return []
+  if (typeof items === 'object' && !Object.keys(items).length) return {}
+  if (Array.isArray(items)) {
+    return items.map(item => deepClone(item))
+  }
+  if (typeof items === 'object') {
+    const newObj = {}
+    for (const key in items) {
+      newObj[key] = deepClone(items[key])
+    }
+    return { ...newObj }
+  }
+  return items
+}
+
+module.exports.deepClone = deepClone
